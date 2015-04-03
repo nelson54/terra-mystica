3e47a381ec3afe9c3dcf3222bf8c45986feeb136
Shafeen M <sodl02@gmail.com>
2015-04-03 17:44:10 -0400

    designed unified ResourceBundle object

--- shafeen/Buildings.py
+++ shafeen/Buildings.py
@@ -7,22 +7,29 @@ class Building(object):
         self.playerOwner = player
         self.faction = player.faction
 
+
 class Dwelling(Building):
     def __init__(self):
         self.upgradeSet = {[TradingHouse.__name__]}
+
 
 class TradingHouse(Dwelling):
     def __init__(self):
         self.upgradeSet = {[Temple.__name__, Stronghold.__name__]}
 
+
 class Stronghold(TradingHouse):
     def __init__(self):
         self.upgradeSet = None
+
 
 class Temple(TradingHouse):
     def __init__(self):
         self.upgradeSet = {[Sanctuary.__name__]}
 
+
 class Sanctuary(Temple):
     def __init__(self):
         self.upgradeSet = None
+
+
